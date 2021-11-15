8839aa5b566b0ff15679ea540eec25fd3a7d9916
mayouli <247067345@qq.com>
2021-11-15 17:42:46 +0800

    列一下函数类型参数的各种写法

--- src/LearnHighOrder.kt
+++ src/LearnHighOrder.kt
@@ -1,13 +1,67 @@
 fun main() {
 
     val learnHighOrder = LearnHighOrder()
+
+    val initPerson = LearnHighOrder.InitPerson()
+
+
+    /**
+     * 直接写lambda表达式
+     */
     learnHighOrder.initPerson {
+        println("直接使用lambda表达式")
         val firstName = "yang"
         val secondName = "chen"
         name = "$firstName $secondName"
         upCaseName()
         age = 11
     }
+
+    /**
+     * 使用声明的lambda表达式
+     */
+    val lambda2 = { person: LearnHighOrder.Person ->
+        person.apply {
+            println("使用声明的lambda表达式")
+            val firstName = "yang"
+            val secondName = "chen"
+            name = "$firstName $secondName"
+            upCaseName()
+            age = 11
+        }
+        Unit
+    }
+
+    learnHighOrder.initPerson(lambda2)
+
+    /**
+     * 实现函数接口
+     */
+    learnHighOrder.initPerson(initPerson)
+
+    /**
+     * 匿名函数实现
+     */
+    learnHighOrder.initPerson(fun(person) {
+        person.apply {
+            println("使用匿名函数")
+            val firstName = "yang"
+            val secondName = "chen"
+            name = "$firstName $secondName"
+            upCaseName()
+            age = 11
+        }
+        return
+    })
+
+//
+//    fun test(block: String.() -> Unit) {
+//        block.invoke("test")
+//    }
+//
+//    test(fun(test: String) {
+//        println(test.length)
+//    })
 }
 
 class LearnHighOrder {
@@ -25,6 +79,21 @@ class LearnHighOrder {
         fun upCaseName() {
             name = name.toUpperCase()
         }
+    }
+
+
+    class InitPerson : (Person) -> Unit {
+        override fun invoke(p1: Person) {
+            println("使用继承函数类型的类")
+            p1.apply {
+                val firstName = "yang"
+                val secondName = "chen"
+                name = "$firstName $secondName"
+                upCaseName()
+                age = 11
+            }
+        }
+
     }
 }
 
